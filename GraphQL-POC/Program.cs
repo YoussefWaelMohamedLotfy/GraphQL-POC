183784081439fbb4d97f3fd6a282683433c8be8c
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-10-04 00:54:29 +0200

    Add project files.

---
+++ GraphQL-POC/Program.cs
@@ -0,0 +1,12 @@
+using GraphQL_POC.Schema;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGraphQLServer()
+    .AddQueryType<Query>();
+
+var app = builder.Build();
+
+app.MapGraphQL();
+
+app.Run();
---
+++ GraphQL-POC/Schema/Query.cs
@@ -0,0 +1,7 @@
+﻿namespace GraphQL_POC.Schema;
+
+public class Query
+{
+    [GraphQLDeprecated("It got old.")]
+    public string Instructions => "Hello from .NET";
+}
