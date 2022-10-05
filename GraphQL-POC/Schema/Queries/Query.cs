b544fbdd2b4cb6b1d08a38e1b44d0fc6419d4ce6
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-10-05 17:03:27 +0200

    Completed Video 8 Pagination

--- GraphQL-POC/Schema/Queries/Query.cs
+++ GraphQL-POC/Schema/Queries/Query.cs
@@ -1,4 +1,5 @@
-﻿using GraphQL_POC.Services.Courses;
+﻿using GraphQL_POC.Data;
+using GraphQL_POC.Services.Courses;
 
 namespace GraphQL_POC.Schema.Queries;
 
@@ -11,7 +12,34 @@ public class Query
         _courseRepository = courseRepository;
     }
 
+    [UsePaging(IncludeTotalCount = true, DefaultPageSize = 10)]
     public async Task<IEnumerable<CourseType>> GetCourses()
+    {
+        var courseDTOs = await _courseRepository.GetAll();
+        return courseDTOs.Select(c => new CourseType
+        {
+            Id = c.Id,
+            Name = c.Name,
+            Subject = c.Subject,
+            InstructorId = c.InstructorId
+        });
+    }
+
+    [UseDbContext(typeof(SchoolDbContext))]
+    [UsePaging(IncludeTotalCount = true, DefaultPageSize = 10)]
+    public IQueryable<CourseType> GetPaginatedCourses([ScopedService] SchoolDbContext context)
+    {
+        return context.Courses.Select(c => new CourseType
+        {
+            Id = c.Id,
+            Name = c.Name,
+            Subject = c.Subject,
+            InstructorId = c.InstructorId
+        });
+    }
+
+    [UseOffsetPaging(IncludeTotalCount = true, DefaultPageSize = 10)]
+    public async Task<IEnumerable<CourseType>> GetOffsetCourses()
     {
         var courseDTOs = await _courseRepository.GetAll();
         return courseDTOs.Select(c => new CourseType
